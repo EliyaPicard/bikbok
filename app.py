8172ccec77a8180a202f0cf32c4c8a5d2730f3cc
EliyaPicard <95577088+EliyaPicard@users.noreply.github.com>
2023-02-17 03:21:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ import io
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
 
 
 @app.route('/result', methods=['POST'])
